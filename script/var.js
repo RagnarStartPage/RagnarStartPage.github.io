78e3fa38bce58a675e2952a262190b6cbdbe823e
Colter <colter.norick@gmail.com>
2017-07-06 10:25:09 -0700

    first commit

---
+++ script/var.js
@@ -0,0 +1,102 @@
+// All the variable for the app
+// Their name are explicit
+
+var username = "Caithir"
+var clientid = "be212a58528168962a39c64052c1d88e";
+var redirecturi = "http://localhost:8002/callback.html";
+var locations = [
+  "12769193",
+  "2383599"
+];
+var images = [
+  "1412446496031.jpg",
+  "1417804954510.jpg",
+  "1422771896804.jpg",
+  "1424655132831.jpg",
+  "1425939966695.jpg",
+  "1429482830109.jpg",
+  "1429569823779.jpg",
+  "1437167260211.jpg",
+  "1437214448937.jpg",
+  "1437214448937.jpg",
+  "1444816416285.jpg",
+  "1444827390885.jpg",
+  "1444827771939.jpg",
+  "1445223016379.jpg",
+  "1445223050369.jpg",
+  "1445370353275.jpg",
+  "1445705808951.jpg",
+  "1445713184723.jpg",
+  "1448399280112.jpg",
+  "1450066383293.jpg",
+  "1450074394745.jpg"
+];
+var searchs = [
+  ["!g", "https://www.google.com/#q="],
+  ["!y", "https://www.youtube.com/results?search_query="],
+  ["!w", "https://en.wikipedia.org/w/index.php?search="]
+];
+var favorites = [
+  [ "Work",
+    [
+      ["github" , "https://github.com/", "gh"],
+      ["gmail" , "https://mail.google.com/mail/u/0/#inbox", "gm"],
+      ["outlook", "https://outlook.office.com/owa/?realm=duke.edu" , "ol"],
+      ["Work Pay" , "https://work.duke.edu/irj/portal/MyPay", "wp"],
+      ["Duke Homepage", "https://my.duke.edu/students/", "hp"]
+    ]
+  ],
+  [ "Social",
+    [
+      ["whatsapp" , "https://web.whatsapp.com/", "wa"],
+      ["hangouts" , "http://hangouts.google.com", "hang"],
+      ["facebook" , "https://www.facebook.com/" , "fb"],
+      ["messenger", "https://www.messenger.com", "ms"]
+    ]
+  ],
+  [ "Useful Rando stuff",
+    [
+     ["Work Music", "https://www.brain.fm/app#!/player/35","bfm"],
+     ["Wolfram", "http://www.wolframalpha.com/", "wf"],
+     ["oeis", "https://oeis.org/", "oeis"],
+     ["Tibetan Dic", "http://www.eng-tib.com/", "td"],
+     ["Amazon", "https://www.amazon.com/", "ama"]
+     
+     ]
+  ],
+  
+  [ "Mind Exploding",
+    [
+      ["RibbonFarm", "http://www.ribbonfarm.com/", "rf"],
+      ["Reservations", "http://unqualified-reservations.blogspot.co.uk/", "rsa"],
+      ["Meaningness", "https://meaningness.com/", "mn"],
+      ["Nuatil", "http://nautil.us/", "nau"]
+    ]
+  ],
+  [ "Reddit",
+    [
+      ["Reddit", "https://www.reddit.com/", "rd"],
+      ["HackerNews", "https://news.ycombinator.com/", "hn"],
+      ["r/All", "https://www.reddit.com/r/all", "ral"],
+      ["Overwatch", "https://www.reddit.com/r/Overwatch/", "row"],
+      ["Programmer Humor", "https://www.reddit.com/r/ProgrammerHumor/", "rph"]
+    ]
+  ],
+  [ "Other/Course Websites",
+    [
+      ["HackerRank", "https://www.hackerrank.com/", "hr"],
+      ["Daily Challenge", "https://www.reddit.com/r/dailyprogrammer/", "dc"],
+      ["Automate Boring Stuff", "https://automatetheboringstuff.com", "abs"],
+      ["alt Music", "http://musicforprogramming.net/?zero", "am"],
+      ["Operating Systems", " https://users.cs.duke.edu/~chase/cps310/", "310"],
+      ["Mobile Design", "http://www.cs.duke.edu/courses/spring17/compsci290.3/", "290"],
+      ["TA Course", "http://www.cs.duke.edu/courses/spring17/compsci249/", "ta"],
+      ["Recorded Lectures", "http://trinity.capture.duke.edu/Panopto/Pages/Sessions/List.aspx?#folderID=\"d4654152-bc9d-4c03-9f46-43642c9cc221\"", "vr"],
+      
+    ]
+  ]
+];
+var feeds = [
+  ["Cool Articles", "http://nautil.us/rss/all"],
+  ["HackerNews", "https://news.ycombinator.com/rss"]
+];
